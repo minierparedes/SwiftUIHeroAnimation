a193d566b8ba00e5095f84bfebc025fa6224facb
minierparedes <3thancr0wn@gmail.com>
2021-04-07 14:59:15 +0900

    Initial Commit

---
+++ HeroAnimation/HeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationApp.swift
+//  HeroAnimation
+//
+//  Created by ethancr0wn on 2021/04/07.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
